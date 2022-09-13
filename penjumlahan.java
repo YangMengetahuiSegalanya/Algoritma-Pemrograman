3df1567c8d958541b57d91c6e8f5a953d2b0e1ff
Surya Agung Saputra <93268577+YangMengetahuiSegalanya@users.noreply.github.com>
2022-09-13 15:52:43 +0700

    Add files via upload

---
+++ penjumlahan.java
@@ -0,0 +1,19 @@
+import.java.util.Scanner;
+
+
+public class penjumlahan {
+    public static void main(string[] args) {
+        int x;
+        int y;
+        Scanner scan = new Scanner(System.in);
+        system.out.print("nilai masukan x =");
+        x = scan.nextInt();
+        System.out.print("masukan nilai y = ");
+        y = scan.nextInt();
+        int hasil = x + y;
+        System.out.print("hasil penjumlahan x + y="+hasil);
+    }
+}
+
+
+
