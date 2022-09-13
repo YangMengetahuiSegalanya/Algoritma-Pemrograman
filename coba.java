b103c7ac4741f68b2dbe31cc9a7d1ad1ef6f8326
Surya Agung Saputra <93268577+YangMengetahuiSegalanya@users.noreply.github.com>
2022-09-13 15:58:05 +0700

    Add files via upload

---
+++ coba.java
@@ -0,0 +1,17 @@
+import.java.util.Scanner;
+
+public class coba {
+    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
+    System.out.print("Masukkan nilai hasil");
+    Scanner = sc.nextFloat();
+    if (hasil >= 60) {
+        System.out.print("Lulus");
+    } 
+    else{
+        System.out.print("Tidak Lulus");
+    }
+}
+}
+
+
