9a0d6632401ed0f3477e4de7facd634366f8c7a2
Surya Agung Saputra <93268577+YangMengetahuiSegalanya@users.noreply.github.com>
2022-09-20 17:19:17 +0700

    Add files via upload

---
+++ prakalgoritma4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prakalgoritma4 {
+    public static  void main(String[] args) {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.print(s= "Masukkan nilai n = ");
+        n = scan.nextInt();
+        int i = 1;
+        while (i<=2*n) {
+            if (i%2==0) {
+                System.out.print(i+" ");
+            }
+            i = i + 1;
+        }
+        System.out.println(x="");
+    }
+}
